5827029277175fb793fde4dccc7060e8c5975926
Odin <odinmanlee@gmail.com>
2015-01-31 20:21:58 +0800

    update

--- workers/localstorage.go
+++ workers/localstorage.go
@@ -1,16 +1,32 @@
 package workers
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
+const (
+	_STORAGE_REDIS  = "redis"
+	_STORAGE_MYSQL  = "mysql"
+	_STORAGE_ORACLE = "oracle"
+)
+
+type StorageOption struct {
+	Type  string
+	Db    string
+	Table string //当Type为mysql/oracle有用
+	Host  string
+	Port  string
+	Pwd   string
+}
+
 type LocalStorage struct {
-	storage string // 存储类型, redis
-	key     string
-	value   string
-	expire  int
+	option *StorageOption // 存储选项
+	key    string
+	value  string
+	expire int
 }
 
 func (w *OmqWorker) localStorage(cmd []string) error {
@@ -21,16 +37,24 @@ func (w *OmqWorker) localStorage(cmd []string) error {
 	if len(cmd) >= 3 {
 		act := strings.ToLower(cmd[0])
 		ls := new(LocalStorage)
-		ls.key = cmd[1]
-		ls.value = cmd[2]
-		if strings.ToLower(ls.key) == "redis" && len(cmd) >= 4 {
-			//兼容旧版,同时意味着redis当有点不安全
-			ls.key = cmd[2]
-			ls.value = cmd[3]
+		option := cmd[1]
+		ls.key = cmd[2]
+		ls.value = cmd[3]
+		if option == "" || strings.ToLower(option) == "redis" {
+			//兼容旧版, 新版应该传入一个json,或者为空
+			ls.option = &StorageOption{Type: _STORAGE_REDIS}
 		} else {
-			if len(cmd) >= 4 {
-				ls.expire, _ = strconv.Atoi(cmd[3])
+			//解析
+			o := new(StorageOption)
+			if err := json.Unmarshal([]byte(option), o); err != nil {
+				w.Info("unmarshal option failed: %s", option)
+				ls.option = &StorageOption{Type: _STORAGE_REDIS} //默认
+			} else {
+				ls.option = o
 			}
+		}
+		if len(cmd) >= 5 {
+			ls.expire, _ = strconv.Atoi(cmd[3])
 		}
 		w.Info("[act: %s][key: %s][value: %s][expire: %d]", act, ls.key, ls.value, ls.expire)
 		switch act {
